c25f5e0c0e30094e2bb5fda269ce672cc5281b97
PoriruaU3A <144504906+PoriruaU3A@users.noreply.github.com>
2026-01-20 18:21:28 +1300

    Update service worker

--- camp/service-worker.js
+++ camp/service-worker.js
@@ -8,13 +8,10 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  // Always go to network during development
+  event.respondWith(fetch(event.request));
 });
