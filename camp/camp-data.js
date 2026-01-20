6dfdbbed1aef907ee4852fa9dd22f64bac3f816b
PoriruaU3A <144504906+PoriruaU3A@users.noreply.github.com>
2026-01-20 17:33:48 +1300

    Update camp data

--- camp/camp-data.js
+++ camp/camp-data.js
@@ -4,56 +4,70 @@ const year_name = "2025";
 
 // Base origin
 const camp_origin = "Hanmer+Springs+TOP+10+Holiday+Park";
-const camp_url = mapsUrl("driving", "Current+Location", camp_origin);
 
 // Monday
 const mon_easy_name = "Hanmer Springs Environs\nEASY";
-const mon_easy_url  = mapsUrl("walking", camp_origin, "-42.527129,172.831639");
+const mon_easy_mode = "walking";
+const mon_easy_dest = "-42.527129,172.831639";
 
 const mon_med_name  = "Mt Isobel to Jacks Pass\nMEDIUM";
-const mon_med_url   = mapsUrl("driving", camp_origin, "-42.488176,172.826614");
+const mon_med_mode  = "driving";
+const mon_med_dest  = "-42.488176,172.826614";
 
 const mon_fit_name  = "Mt Isobel Dog Stream Waterfall\nFIT";
-const mon_fit_url   = mapsUrl("driving", camp_origin, "-42.488176,172.826614");
+const mon_fit_mode  = "driving";
+const mon_fit_dest  = "-42.488176,172.826614";
 
 // Tuesday
 const tue_easy_name = "Option Dog Stream Circuit\nEASY";
-const tue_easy_url  = mapsUrl("walking", camp_origin, "-42.527129,172.831639");
+const tue_easy_mode = "walking";
+const tue_easy_dest = "-42.527129,172.831639";
 
 const tue_med_name  = "Mt Dumblane\nMEDIUM";
-const tue_med_url   = mapsUrl("driving", camp_origin, "-42.488176,172.826614");
+const tue_med_mode  = "driving";
+const tue_med_dest  = "-42.488176,172.826614";
 
 const tue_fit_name  = "Mt Dumblane and Tops\nFIT";
-const tue_fit_url   = mapsUrl("driving", camp_origin, "-42.488176,172.826614");
+const tue_fit_mode  = "driving";
+const tue_fit_dest  = "-42.488176,172.826614";
 
 // Wednesday
 const wed_easy_name = "Workin Bee at Boyle";
-const wed_easy_url  = mapsUrl("driving", camp_origin, "St+James+Walkway+Car+Park");
+const wed_easy_mode = "driving";
+const wed_easy_dest = "St+James+Walkway+Car+Park";
 
 const wed_med_name  = "St James Walkway Car Park";
-const wed_med_url   = mapsUrl("driving", camp_origin, "St+James+Walkway+Car+Park");
+const wed_med_mode  = "driving";
+const wed_med_dest  = "St+James+Walkway+Car+Park";
 
 const wed_fit_name  = "Cannibal Gorge";
-const wed_fit_url   = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const wed_fit_mode  = "driving";
+const wed_fit_dest  = "-42.37368053104093,172.37790006192594";
 
 // Thursday
 const thu_easy_name = "Cannibal Gorge Track\nSluice Box\nLewis Pass Alpine walk\nEASY";
-const thu_easy_url  = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const thu_easy_mode = "driving";
+const thu_easy_dest = "-42.37368053104093,172.37790006192594";
 
 const thu_med_name  = "St James Walkway\ntowards Gorge Hut\nMEDIUM";
-const thu_med_url   = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const thu_med_mode  = "driving";
+const thu_med_dest  = "-42.37368053104093,172.37790006192594";
 
 const thu_fit_name  = "Lewis Tops\nFIT";
-const thu_fit_url   = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const thu_fit_mode  = "driving";
+const thu_fit_dest  = "-42.37368053104093,172.37790006192594";
 
 // Friday
 const fri_easy_name = "St James Homestead\nPeters Track\nEASY";
-const fri_easy_url  = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const fri_easy_mode = "driving";
+const fri_easy_dest = "-42.37368053104093,172.37790006192594";
 
 const fri_med_name  = "Ghost Spur Track to Rocky\nLookout and Kam Point\nMEDIUM";
-const fri_med_url   = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const fri_med_mode  = "driving";
+const fri_med_dest  = "-42.37368053104093,172.37790006192594";
 
 const fri_fit_name  = "Ghost Spur Track and Ghost\nLink to Ghost Rock\nFIT";
-const fri_fit_url   = mapsUrl("driving", camp_origin, "-42.37368053104093,172.37790006192594");
+const fri_fit_mode  = "driving";
+const fri_fit_dest  = "-42.37368053104093,172.37790006192594";
 
 //=========================================== END CONFIG ============================================
