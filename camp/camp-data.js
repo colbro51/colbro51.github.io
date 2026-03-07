f9a22f5c52c27e5012d39bcf26e3bb6607ac845d
PoriruaU3A <144504906+PoriruaU3A@users.noreply.github.com>
2026-03-07 17:06:34 +1300

    Update to 2026

--- camp/camp-data.js
+++ camp/camp-data.js
@@ -2,8 +2,8 @@
 // CAMP CONFIGURATION
 // ===========================================================
 
-export const year_name = "2025";
-export const camp_location = "-42.528150744255456, 172.82987346924634";
+export const year_name = "2026";
+export const camp_location = "-38.9931004,175.8131507";
 
 // ===========================================================
 // DAY ROUTES (structured by day → difficulty → {name, mode, dest})
@@ -12,35 +12,35 @@ export const camp_location = "-42.528150744255456, 172.82987346924634";
 export const days = {
   mon: {
     easy: {
-      name: "Hanmer Springs Environs\nEASY",
-      mode: "walking",
-      dest: "-42.527129,172.831639"
+      name: "Taranaki Falls, Silica Rapids & Meads Wall\nTongariro National Park (Whakapapa area)\nEASY/MED [$13/$38]",
+      mode: "driving",
+      dest: "-39.199563, 175.542822"
     },
     med: {
-      name: "Mt Isobel to Jacks Pass\nMEDIUM [$6]",
+      name: "Taranaki Falls and Lower Tama Lake\nTongariro National Park (Whakapapa area)\nMEDIUM [$13/$38]",
       mode: "driving",
-      dest: "-42.488176,172.826614"
+      dest: "-39.199563, 175.542822"
     },
     fit: {
-      name: "Mt Isobel Dog Stream Waterfall\nFIT [$6]",
+      name: "Whakapapaiti Valley Track\nTongariro National Park (Whakapapa area)\nFIT [$15/$44]",
       mode: "driving",
-      dest: "-42.488176,172.826614"
+      dest: "-39.198388, 175.537557"
     }
   },
 
   tue: {
     easy: {
-      name: "Option Dog Stream Circuit\nEASY",
-      mode: "walking",
+      name: "Waihohonu Historic Hut\nTongariro National Park (eastern)\nEASY/MED [$9/$28]",
+      mode: "driving",
       dest: "-42.527129,172.831639"
     },
     med: {
-      name: "Mt Dumblane\nMEDIUM [$6]",
+      name: "Waihohonu Historic Hut & Ohinepango Springs\nTongariro National Park (eastern)\nMEDIUM [$9/$28]",
       mode: "driving",
       dest: "-42.488176,172.826614"
     },
     fit: {
-      name: "Mt Dumblane and Tops\nFIT [$6]",
+      name: "Waihohonu, Ohinepango Springs, Historic Hut -> Oturere Hut\nTongariro National Park (eastern)\nFIT [$9/$28]",
       mode: "driving",
       dest: "-42.488176,172.826614"
     }
@@ -48,35 +48,35 @@ export const days = {
 
   wed: {
     easy: {
-      name: "Workin Bee",
+      name: "Workin Bee\n[$3/$10]",
       mode: "driving",
-      dest: "St+James+Walkway+Car+Park"
+      dest: "-38.913342, 175.899195"
     },
     med: {
-      name: "Workin Bee",
+      name: "Workin Bee\n[$3/$10]",
       mode: "driving",
-      dest: "St+James+Walkway+Car+Park"
+      dest: "-38.913342, 175.899195"
     },
     fit: {
-      name: "Workin Bee",
+      name: "Workin Bee\n[$3/$10]",
       mode: "driving",
-      dest: "St+James+Walkway+Car+Park"
+      dest: "-38.913342, 175.899195"
     }
   },
 
   thu: {
     easy: {
-      name: "Cannibal Gorge Track\nSluice Box\nLewis Pass Alpine walk\nEASY [$17]",
+      name: "Pillars of Hercules, Tree Trunk Gorge (part)\nand Kaimanawa Forest Loop\nEASY/EASY MED [$6/$17]",
       mode: "driving",
       dest: "-42.37368053104093,172.37790006192594"
     },
     med: {
-      name: "St James Walkway\ntowards Gorge Hut\nMEDIUM [$14]",
+      name: "Tree Trunk Gorge,  Pillars of Hercules\nKaimanawa Forest Park + optional loop\nMEDIUM [$6/$17]",
       mode: "driving",
       dest: "-42.37368053104093,172.37790006192594"
     },
     fit: {
-      name: "Lewis Tops\nFIT [$14]",
+      name: "Urchin Tops, Kaimanawa Forest Park\nFIT [$6/$17]",
       mode: "driving",
       dest: "-42.37368053104093,172.37790006192594"
     }
@@ -84,19 +84,19 @@ export const days = {
 
   fri: {
     easy: {
-      name: "St James Homestead\nPeters Track\nEASY [$11]",
-      mode: "driving",
-      dest: "-42.37368053104093,172.37790006192594"
+      name: "Tongariro River Trail\n(Turangi to Red Hut Bridge)\nEASY/EASY MED",
+      mode: "walking",
+      dest: "-38.993835, 175.815055"
     },
     med: {
-      name: "Ghost Spur Track to Rocky\nLookout and Kam Point\nMEDIUM [$11]",
+      name: "Rotopounamu & Tongariro River Trail\n(Turangi to Red Hut Bridge)\nMEDIUM [$3/$10]",
       mode: "driving",
-      dest: "-42.37368053104093,172.37790006192594"
+      dest: "-39.016742, 175.732029"
     },
     fit: {
-      name: "Ghost Spur Track and Ghost\nLink to Ghost Rock\nFIT [$11]",
+      name: "Tama Lakes & Taranaki Falls\nTongariro National Park (Whakapapa)\nFIT [$13/$38]",
       mode: "driving",
-      dest: "-42.37368053104093,172.37790006192594"
+      dest: "-39.199570, 175.542789"
     }
   }
 };
@@ -107,20 +107,34 @@ export const days = {
 
 export const more = [
   {
-    name: "Tuesday meal 7:15pm",
+    name: "Scoria Flat [Monday Fit]",
     mode: "driving",
-    dest: "6 Jacks Pass Rd"
+    dest: "-39.230715, 175.548709"
   },
-  {
-    name: "Friday meal 5:30pm",
+  { name: "[Sun] Turangi Bridge Bar/Restaurant 6:45",
+    mode: "walking",
+    dest: "-38.986765, 175.817857"
+  },
+  { name: "[Tue] Riverside Restaurant & Bar 6:45pm",
+    mode: "walking",
+    dest: "-38.992267, 175.810113"
+  },
+  { name: "Rotopounamu Track",
     mode: "driving",
-    dest: "1 Conical Hill Rd, Hanmer Springs"
+    dest: "-39.016742, 175.732029"
   },
-  { name: "", mode: "", dest: "" },
-  { name: "", mode: "", dest: "" },
-  { name: "", mode: "", dest: "" },
-  { name: "", mode: "", dest: "" },
-  { name: "", mode: "", dest: "" },
+  { name: "Tokaanu Thermal Pools",
+    mode: "driving",
+    dest: "-38.967157, 175.764838"
+  },
+  { name: "Pihanga Medical Centre, Turangi",
+    mode: "driving",
+    dest: "-38.988081, 175.807477"
+  },
+  { name: "Taupo Hospital",
+    mode: "driving",
+    dest: "-38.698788, 176.097192"
+  },
   { name: "", mode: "", dest: "" },
   { name: "", mode: "", dest: "" },
   { name: "", mode: "", dest: "" }
